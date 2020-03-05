488d4be6b78ca18ded797f42f55ffb218139743e
Ilja <i.lichtenberg@tallence.com>
2020-03-05 15:15:04 +0100

    hover => Farbe wird dunkler To Do Liste erstellt Checkboxes erstellt Zustände (true und false) => einsortierung rechts und links grid erweitert 2 way data bounding Kommunikation zum Back End

--- src/app/todolist-box/todolist-box.component.ts
+++ src/app/todolist-box/todolist-box.component.ts
@@ -8,7 +8,8 @@ import {HttpClient} from '@angular/common/http';
 })
 export class TodolistBoxComponent implements OnInit {
   listTitle = 'Work';
-  todos = ['finish project', 'call client', 'start new project']
+  todos: ToDoList[] = [];
+  isCreatingTodo = false;
 
   constructor(private http: HttpClient) {
   }
@@ -17,7 +18,10 @@ export class TodolistBoxComponent implements OnInit {
     this.getToDos().subscribe(data => {
       console.table(data);
       console.log('received data');
-      this.todos = data;
+      data.forEach(todoText => {
+        const newTodo = new ToDoList(todoText, false);
+        this.todos.push(newTodo);
+      });
     });
   }
 
@@ -25,4 +29,69 @@ export class TodolistBoxComponent implements OnInit {
     return this.http.get<string[]>('http://localhost:1908/todo/show/2');
   }
 
+  handleClick(textt: any) {
+    const todo = new ToDoRequestData();
+    todo.id = 2;
+    todo.text = textt;
+
+    this.http.post('http://localhost:1908/todo/create', todo).subscribe(none => {
+      console.log('created todo');
+      this.isCreatingTodo = false;
+
+      this.getToDos().subscribe(data => {
+        console.table(data);
+        console.log('received data');
+        data.forEach(todoText => {
+          const newTodo = new ToDoList(todoText, false);
+          this.todos.push(newTodo);
+        });
+      });
+    });
+  }
+
+  getCompletedTodos() {
+    const completed = [];
+
+    this.todos.forEach(todo => {
+      if (todo.condition === true) {
+        completed.push(todo);
+      }
+    });
+
+    return completed;
+
+  }
+
+  getNonCompletedTodos() {
+    const noncompleted = [];
+
+    this.todos.forEach(todo => {
+      if (todo.condition === false) {
+        noncompleted.push(todo);
+      }
+    });
+
+    return noncompleted;
+  }
+
+  complete(todo: ToDoList) {
+    todo.condition = true;
+  }
+}
+
+class ToDoRequestData {
+  text: string;
+  id: number;
+}
+
+class ToDoList {
+  text: string;
+  condition: boolean;
+  creator: any;
+
+
+  constructor(text: string, condition: boolean) {
+    this.text = text;
+    this.condition = condition;
+  }
 }
